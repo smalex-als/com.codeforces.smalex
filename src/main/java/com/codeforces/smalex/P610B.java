6b3b82acbdaf49d1e86c5f56662073f66dd3ec4e
Alexey Smychagin <smalex69@gmail.com>
2016-11-09 13:55:13 -0500

    add 610 & 625 remove unused

---
+++ src/main/java/com/codeforces/smalex/P610B.java
@@ -0,0 +1,191 @@
+package com.codeforces.smalex;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+
+public class P610B {
+  // problem http://codeforces.com/contest/610/problem/B
+  InputStream is;
+  PrintWriter out;
+  String INPUT = "6\n" + 
+    "10 10 10 1 10 10";
+  
+  void solve() {
+    int n = ni();
+    int[] a = new int[n];
+    int min = Integer.MAX_VALUE;
+    for (int i = 0; i < n; i++) {
+      a[i] = ni();
+      min = Math.min(a[i], min);
+    }
+    long res = (long) n * min;
+    int best = 0;
+    int cnt = 0;
+    int start = 0;
+    Map<Integer, Integer> sum = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      if (a[i] == min) {
+        if (cnt > 0) {
+          sum.put(start, cnt);
+        }
+        cnt = 0;
+      } else {
+        if (cnt == 0) {
+          start = i;
+        }
+        cnt++;
+      }
+    }
+    if (cnt > 0) {
+      if (sum.containsKey(0)) {
+        cnt += sum.get(0);
+      }
+      sum.put(start, cnt);
+    }
+
+    for (Integer s : sum.keySet()) {
+      best = Math.max(sum.get(s), best);
+    }
+    System.out.println(res + best);
+  }
+  
+  public static void main(String[] args) throws Exception {
+    new P610B().run();
+  }
+  
+  void run() throws Exception {
+    is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+    out = new PrintWriter(System.out);
+  
+    long s = System.currentTimeMillis();
+    solve();
+    out.flush();
+    tr(System.currentTimeMillis() - s + "ms");
+  }
+  
+  private byte[] inbuf = new byte[1024];
+  private int lenbuf = 0, ptrbuf = 0;
+  
+  private int readByte() {
+    if (lenbuf == -1)
+      throw new InputMismatchException();
+    if (ptrbuf >= lenbuf) {
+      ptrbuf = 0;
+      try {
+        lenbuf = is.read(inbuf);
+      } catch (IOException e) {
+        throw new InputMismatchException();
+      }
+      if (lenbuf <= 0)
+        return -1;
+    }
+    return inbuf[ptrbuf++];
+  }
+  
+  private boolean isSpaceChar(int c) {
+    return !(c >= 33 && c <= 126);
+  }
+  
+  private int skip() {
+    int b;
+    while ((b = readByte()) != -1 && isSpaceChar(b))
+      ;
+    return b;
+  }
+  
+  private double nd() {
+    return Double.parseDouble(ns());
+  }
+  
+  private char nc() {
+    return (char) skip();
+  }
+  
+  private String ns() {
+    int b = skip();
+    StringBuilder sb = new StringBuilder();
+    while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b != ' ')
+      sb.appendCodePoint(b);
+      b = readByte();
+    }
+    return sb.toString();
+  }
+  
+  private char[] ns(int n) {
+    char[] buf = new char[n];
+    int b = skip(), p = 0;
+    while (p < n && !(isSpaceChar(b))) {
+      buf[p++] = (char) b;
+      b = readByte();
+    }
+    return n == p ? buf : Arrays.copyOf(buf, p);
+  }
+  
+  private char[][] nm(int n, int m) {
+    char[][] map = new char[n][];
+    for (int i = 0; i < n; i++)
+      map[i] = ns(m);
+    return map;
+  }
+  
+  private int[] na(int n) {
+    int[] a = new int[n];
+    for (int i = 0; i < n; i++)
+      a[i] = ni();
+    return a;
+  }
+  
+  private int ni() {
+    int num = 0, b;
+    boolean minus = false;
+    while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+      ;
+    if (b == '-') {
+      minus = true;
+      b = readByte();
+    }
+  
+    while (true) {
+      if (b >= '0' && b <= '9') {
+        num = num * 10 + (b - '0');
+      } else {
+        return minus ? -num : num;
+      }
+      b = readByte();
+    }
+  }
+  
+  private long nl() {
+    long num = 0;
+    int b;
+    boolean minus = false;
+    while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+      ;
+    if (b == '-') {
+      minus = true;
+      b = readByte();
+    }
+  
+    while (true) {
+      if (b >= '0' && b <= '9') {
+        num = num * 10 + (b - '0');
+      } else {
+        return minus ? -num : num;
+      }
+      b = readByte();
+    }
+  }
+  
+  private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+  
+  private void tr(Object... o) {
+    if (!oj)
+      System.out.println(Arrays.deepToString(o));
+  }
+}
--- src/main/java/com/codeforces/smalex/P633C2.java
+++
@@ -1,7 +0,0 @@
-package com.codeforces.smalex;
-
-/**
- * Created by smalex on 27/02/16.
- */
-public class P633C2 {
-}
